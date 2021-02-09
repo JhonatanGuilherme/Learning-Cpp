a31fa90b84dbcb7f4b9f073c31f486e0310af92b
Jhonatan Oliveira <oliveirajhon.g@gmail.com>
2021-02-09 13:51:49 -0300

    Dynamic memory allocation;

---
+++ Data Structure/39_AlocaçãoDinâmicaDeMemória.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h> // Para função fgets;
+#include <stdlib.h> // Para função malloc;
+#include <iostream>
+#include <locale>
+
+using namespace std;
+
+int main() {
+  setlocale(LC_ALL, "pt_BR.UTF-8"); // Funcionou perfeitamente;
+  char *nome;
+
+  nome = (char *) malloc(sizeof(char));
+
+  //cin >> nome; // Retorna somente a primeira palavra antes do Enter ou Espaço;
+  //fgets(nome, sizeof(nome), stdin);
+  cin.get(nome, sizeof(nome));
+  cout << nome;
+
+  cout << flush; // Esvaziando o buffer de saída;
+
+  return 0;
+}
